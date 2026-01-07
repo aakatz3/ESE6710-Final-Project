52da2fc9a16a245a54b3975b3f0d70ec8b8e1d73
Andrew Katz <aakatz3@gmail.com>
2026-01-07 17:37:30 -0500

    Update paper

--- calcs/mechdesign.m
+++ calcs/mechdesign.m
@@ -13,6 +13,11 @@ vpa(pcbHolderHeight) %[output:76c815c7]
 
 coilHolderHeight = bracket + acrylic %[output:16869a2c]
 vpa(coilHolderHeight) %[output:7e50eba6]
+%%
+%[text] Measurement math
+round(separateUnits(unitConvert(1.3845*u.in + 3*u.mm,u.in)),3) %[output:877c8240]
+
+1.41-0.058+0.0682 %[output:0700c414]
 
 %[appendix]{"version":"1.0"}
 %---
@@ -20,26 +25,32 @@ vpa(coilHolderHeight) %[output:7e50eba6]
 %   data: {"layout":"onright"}
 %---
 %[output:03ce8462]
-%   data: {"dataType":"symbolic","outputData":{"name":"nut","value":"\\frac{8001}{2500}\\,{\\textrm{mm}}"}}
+%   data: {"dataType":"symbolic","outputData":{"name":"nut","value":"\\frac{8001}{2500}\\,\\mathrm{mm}"}}
 %---
 %[output:3d6edfd7]
-%   data: {"dataType":"symbolic","outputData":{"name":"channel","value":"\\frac{9}{5}\\,{\\textrm{mm}}"}}
+%   data: {"dataType":"symbolic","outputData":{"name":"channel","value":"\\frac{9}{5}\\,\\mathrm{mm}"}}
 %---
 %[output:54ce7456]
-%   data: {"dataType":"symbolic","outputData":{"name":"acrylic","value":"6\\,{\\textrm{mm}}"}}
+%   data: {"dataType":"symbolic","outputData":{"name":"acrylic","value":"6\\,\\mathrm{mm}"}}
 %---
 %[output:5db60bc4]
-%   data: {"dataType":"symbolic","outputData":{"name":"bracket","value":"\\frac{11}{2}\\,{\\textrm{mm}}"}}
+%   data: {"dataType":"symbolic","outputData":{"name":"bracket","value":"\\frac{11}{2}\\,\\mathrm{mm}"}}
 %---
 %[output:53707711]
-%   data: {"dataType":"symbolic","outputData":{"name":"pcbHolderHeight","value":"\\frac{27501}{2500}\\,{\\textrm{mm}}"}}
+%   data: {"dataType":"symbolic","outputData":{"name":"pcbHolderHeight","value":"\\frac{27501}{2500}\\,\\mathrm{mm}"}}
 %---
 %[output:76c815c7]
-%   data: {"dataType":"symbolic","outputData":{"name":"ans","value":"11.0004\\,{\\textrm{mm}}"}}
+%   data: {"dataType":"symbolic","outputData":{"name":"ans","value":"11.0004\\,\\mathrm{mm}"}}
 %---
 %[output:16869a2c]
-%   data: {"dataType":"symbolic","outputData":{"name":"coilHolderHeight","value":"\\frac{23}{2}\\,{\\textrm{mm}}"}}
+%   data: {"dataType":"symbolic","outputData":{"name":"coilHolderHeight","value":"\\frac{23}{2}\\,\\mathrm{mm}"}}
 %---
 %[output:7e50eba6]
-%   data: {"dataType":"symbolic","outputData":{"name":"ans","value":"11.5\\,{\\textrm{mm}}"}}
+%   data: {"dataType":"symbolic","outputData":{"name":"ans","value":"11.5\\,\\mathrm{mm}"}}
+%---
+%[output:877c8240]
+%   data: {"dataType":"symbolic","outputData":{"name":"ans","value":"1.503"}}
+%---
+%[output:0700c414]
+%   data: {"dataType":"textualVariable","outputData":{"name":"ans","value":"1.4202"}}
 %---
